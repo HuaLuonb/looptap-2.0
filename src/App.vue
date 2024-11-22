e7cf6d69e9dd4e61617e308d06ff989e37f7e1e0
HuaLuonb <168878621+HuaLuonb@users.noreply.github.com>
2024-11-21 19:48:52 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,46 @@
-<template> <div> <h1>Hello Vue 3 + Vite</h1> <p>Start editing to see some magic happen!</p> </div> </template> <script> export default { name: "App", }; </script> <style> h1 { color: #42b983; } </style>
+<template>
+  <div class="app">
+    <h1>Loop Tap</h1>
+    <input 
+      v-model="developerCode" 
+      placeholder="开发者代码" 
+      type="password"
+      @keyup.enter="checkDeveloperCode"
+    >
+    <button @click="checkDeveloperCode">进入</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      developerCode: ''
+    }
+  },
+  methods: {
+    checkDeveloperCode() {
+      if (this.developerCode === '54188') {
+        console.log('开发者模式')
+      } else {
+        alert('开发者代码错误')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+input, button {
+  margin: 10px;
+  padding: 5px;
+}
+</style>
